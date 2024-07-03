ff3a6a7411b5dd68936027c40c1958e39edca2f4
gabrielrez <97976458+gabrielrez@users.noreply.github.com>
2024-07-03 11:04:35 -0300

    prevent from sql injection

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
+
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
 require 'Database.php';
 
-// $config = require 'config.php';
-// $db = new Database($config['database']);
-// $posts = $db->query('SELECT * FROM posts')->fetchAll(PDO::FETCH_ASSOC);
+$config = require 'config.php';
+$db = new Database($config['database']);
 
-// dd($posts);
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
+dd($posts);
